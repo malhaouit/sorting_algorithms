e6793bd62bba9b2d7c5eeb5113ff24c5846c4614
malhaouit <abdelmounaim.abdel@gmail.com>
2023-12-22 00:24:06 -0800

    Update: Reduce the number of iterations and print the array after each swaping

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,17 +15,17 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = temp;
+				print_array(array, size);
 			}
 		}
-		print_array(array, size);
 	}
 }
